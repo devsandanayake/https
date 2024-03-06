2006eb070cecd3856aa3e6526e473bbc76eedfea
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-03-07 01:29:33 +0530

    up

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const options = {
     cert: fs.readFileSync('cert/server.cert')
 };
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
 
-server.listen(port,() => {
+app.listen(port,() => {
     console.log(`Server is running on HTTPS protocol`);
 });
